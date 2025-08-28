fa4cbb3e18ffe599913c6f6d2b9d0cbc389940f0
adam-dev <adammikaildevs@gmail.com>
2025-08-28 21:10:49 +0800

    update

--- src/app/api/apitest/testOracle/route.ts
+++ src/app/api/apitest/testOracle/route.ts
@@ -1,25 +1,26 @@
 
 import { NextRequest, NextResponse } from "next/server";
-import oracledb from "oracledb";
+import { getOracleConnection } from "@/lib/oracledb";
 
 export async function GET() {
     try {
         // Point to your Instant Client directory
-        oracledb.initOracleClient({ libDir: "C:\\oracle\\instantclient_19_23" });
+        // oracledb.initOracleClient({ libDir: "/opt/oracle/instantclient_19_28" });
         // oracledb.initOracleClient({ libDir: undefined });
 
-        const connection = await oracledb.getConnection({
-            user: "gev",
-            password: "gev2025!",
-            connectString: "192.168.199.227:1521/his"
-            // user: "system",
-            // password: "MyPassword123",
-            // connectString: "192.168.151.190:1521/FREEPDB1",
-        });
+        let conn = await getOracleConnection();
+        // const connection = await conn.execute({
+        //     user: "gev",
+        //     password: "gev2025!",
+        //     connectString: "192.168.199.227:1521/his"
+        //     // user: "system",
+        //     // password: "MyPassword123",
+        //     // connectString: "192.168.151.190:1521/FREEPDB1",
+        // });
 
-        const result = await connection.execute("SELECT * FROM HIS.MID002ANALYZER_RESULT");
+        const result = await conn.execute("SELECT * FROM HIS.MID002ANALYZER_RESULT");
 
-        await connection.close();
+        await conn.close();
 
         return NextResponse.json({ success: true, data: result.rows }, { status: 200 });
     } catch (err) {
